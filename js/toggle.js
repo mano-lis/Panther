989616ba27c749bc91e2b5786c8d1a939cd6651e
manolis <laptit99@hotmail.fr>
2023-01-26 12:19:53 +0100

    remove useless file

--- js/toggle.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('#nav-toggle');
-const navBar = document.querySelector('.navbar-1');
-
-btn.addEventListener('click', () => {
-    console.log('here');
-    navBar.classList.toggle('in'); 
-});
